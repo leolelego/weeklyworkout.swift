14d1e0c800cd9fc3f919fc63d7c676c6c6a19c9d
Léo <4069471+leolelego@users.noreply.github.com>
2024-01-19 16:56:03 +0100

    Initial Commit

---
+++ HealthData/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HealthData
+//
+//  Created by Léo on 19/01/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
